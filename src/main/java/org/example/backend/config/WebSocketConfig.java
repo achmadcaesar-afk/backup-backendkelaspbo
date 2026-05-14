bc1cf07e33648a3c55bd26e261752e07b00117b7
Ahmadmapple <Ahmadmapple@gmail.com=>
2026-05-15 00:21:40 +0700

    Benerin bug

--- src/main/java/org/example/backend/config/WebSocketConfig.java
+++ src/main/java/org/example/backend/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package org.example.backendkelaspbo.config;
+package org.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
